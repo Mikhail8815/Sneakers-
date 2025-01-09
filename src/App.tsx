9b9e4271ff3ad3ade58a0ade9e1cf3a1452e850c
Mikhail8815 <mishutka150588@mail.ru>
2025-01-09 23:03:00 +0300

    NavLink

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import styles from "./components/Site.module.css";
 import {PageOne} from "./components/pages/PageOne";
 import {PageTwo} from "./components/pages/PageTwo";
 import {PageThree} from "./components/pages/PageThree";
-import {Navigate, NavLink, Route, Routes} from "react-router-dom";
+import {Navigate, Route, Routes} from "react-router-dom";
 import {Error404} from "./components/pages/Error404";
 
 
@@ -13,9 +13,7 @@ function App() {
             <div className={styles.header}><h1>HEADER</h1></div>
             <div className={styles.body}>
                 <div className={styles.nav}>
-                    <div><NavLink to={"/page1"}>Page 1</NavLink></div>
-                    <div><NavLink to={"/page2"}>Page 2</NavLink></div>
-                    <div><NavLink to={"/page3"}>Page 3</NavLink></div>
+                    Здесь будет навигация
                 </div>
                 <div className={styles.content}>
                     <Routes>
@@ -28,6 +26,9 @@ function App() {
                         <Route path="/error404" element={<Error404 />} />
                         <Route path="/*" element={<Navigate to={"/error404"} />} />
                     </Routes>
+                    {/*<PageOne/>*/}
+                    {/*<PageTwo/>*/}
+                    {/*<PageThree/>*/}
                 </div>
             </div>
             <div className={styles.footer}>abibas 2023</div>
